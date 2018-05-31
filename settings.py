cfeb18c8a6a62687cfd8c74a4d3ae35a5db36434
RokHyung Son <fhrgud1264@gmail.com>
2018-05-31 16:04:20 +0900

    If user don't have sound card, Don't load sound effects

--- settings.py
+++ settings.py
@@ -21,6 +21,7 @@ class Settings():
         self.bullets_allowed = 3
 
         try:
+            self_have_sound = True
             self.shot_sound = pygame.mixer.Sound('sounds/shoot.wav')
             self.alien_die_sound = pygame.mixer.Sound('sounds/invaderkilled.wav')
             self.die_sound = pygame.mixer.Sound('sounds/explosion.wav')
