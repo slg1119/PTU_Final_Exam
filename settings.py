d33402d23cde32fcf0b8a2617704a06fc6b604cd
RokHyung Son <fhrgud1264@gmail.com>
2018-06-09 13:22:58 +0900

    Resize bullet width

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ class Settings():
         self.have_sound = True
             
         # Bullet settings.
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0, 255, 0)
         self.bullets_allowed = 3
